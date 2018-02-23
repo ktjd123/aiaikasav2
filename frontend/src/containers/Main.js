3f1e56b25d59261a98a20353c46d072326540439
ktjd1262 <k9326239@gmail.com>
2018-02-23 20:22:39 +0900

    update

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import {MainTemplate, Info} from 'components'
+import {toast} from 'react-toastify'
 
 class Main extends Component {
+    componentDidMount() {
+        toast.success('react-toastify')
+    }
+    
     render() {
         return (
             <div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Main} from 'containers'
+import { Main } from 'containers'
 import registerServiceWorker from './registerServiceWorker';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {ToastContainer} from 'react-toastify'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.render(
     <BrowserRouter>
-        <Switch>
-            <Route path='/' component={Main} />
-        </Switch>
+        <div>
+            <ToastContainer
+                autoClose={3000}
+                position="bottom-center"
+            />
+            <Switch>
+                <Route path='/' component={Main} />
+            </Switch>
+        </div>
     </BrowserRouter>
     , document.getElementById('root'));
 registerServiceWorker();
