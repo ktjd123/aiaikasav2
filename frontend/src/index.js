f99aecf45cdbc100a1a19f50e39503fef152fd41
ktjd1262 <k9326239@gmail.com>
2018-02-23 20:19:51 +0900

    frontend update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import {Main} from 'containers'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' component={Main} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
