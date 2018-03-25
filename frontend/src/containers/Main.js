64ce5d004aa3bc17d508f0daea29d99a14bb0e2c
ktjd1262 <k9326239@gmail.com>
2018-03-26 01:20:27 +0900

    toastify update

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import {Header, MainTemplate, Info} from 'components'
-import {toast} from 'react-toastify'
 
 class Main extends Component {
-    componentDidMount() {
-        toast.success('react-toastify')
-    }
-    
     render() {
         return (
             <div>
                 <Header/>
-                {/* todo robots.txt */}
                 <MainTemplate
                     info={<Info/>}
                 />
