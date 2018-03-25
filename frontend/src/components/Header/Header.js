334f674f36dfdf358076f2dcf82a99312eb2cb8e
ktjd1262 <k9326239@gmail.com>
2018-03-26 00:45:49 +0900

    header update

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import FaSearch from 'react-icons/lib/fa/search'
+import './Header.css'
 
 class Header extends Component {
+    
+    state = {
+        isFocus: false
+    }
+
+    toggleFocus = () => {
+        this.setState({
+            isFocus: !this.state.isFocus
+        })
+    }
+
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <header>
+                <Link to='/' className='logo' onClick={() => window.scroll(0,0)} >AIAIKASA</Link>
+                <section className='search'>
+                   <div className={`searchBox ${this.state.isFocus ? 'focus': undefined}`}>
+                        <i><FaSearch/></i>
+                        <input type='text' 
+                        placeholder='모든걸 검색해보세요' 
+                        onFocus={this.toggleFocus} 
+                        onBlur={this.toggleFocus} 
+                        className={this.state.isFocus ? 'focus' : undefined}
+                        />
+                   </div> 
+                </section>
+                <section className='menus'>
+                    lorem ipsum
+                </section>
+            </header>
         );
     }
 }
